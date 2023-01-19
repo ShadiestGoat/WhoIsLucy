347efcb6ef250132cb78aa4a3fb9df514a377f97
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2023-01-19 20:52:17 +0000

    config

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
+export default {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			out: 'build',
+			precompress: false,
+			envPrefix: ''
+		}),
 	}
 };
-
-export default config;
